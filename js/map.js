7cbffeb2e15803c8a8af1f7eb6454b7c1a5adfab
Abdallah Nasser <abdallah.n.helal@gmail.com>
2017-09-11 19:19:26 +0200

    edits done

--- js/map.js
+++ js/map.js
@@ -3,79 +3,9 @@ var map;
 // Use flicker.com api
 var filcker = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=306d8113632c74aeb2a02482dc3a909d&format=json&text=";
 
-// Chosen places that will appear on map
-// Those places are selected in Cairo, Egypt
-var nearby_places = [
-    {
-        place_name: "Al-Ahly Sporting Club",
-        location: {lat: 30.048484, lng:  31.222244},
-        description: "Al Ahly Sporting Club, is an Egyptian sports club based in Cairo," +
-        " Egypt. It is best known for its professional football team, " +
-        "which plays in the Egyptian Premier League, the top tier in " +
-        "the Egyptian football league system."
-    },
-    {
-        place_name: "Cairo Tower",
-        location: {lat: 30.045930, lng: 31.224250},
-        description: "The Cairo Tower - commonly known to locals" +
-        " as \"Nasser's Pineapple\" - is a free-standing concrete" +
-        " tower located in Cairo, Egypt. At 187 m, it has been the" +
-        " tallest structure in Egypt and North Africa for about 50 years."
-    },
-    {
-        place_name: "El sawy Culture Wheel",
-        location: {lat: 30.061957, lng: 31.217200},
-        description: "El Sawy Culture Wheel is an all-purpose, private" +
-        " cultural center, located on Gezira Island in the Zamalek" +
-        " district, central Cairo, Egypt. It is considered one of " +
-        "the most important cultural venues in Egypt."
-    },
-    {
-        place_name: "Cairo Opera House",
-        location: {lat: 30.042779, lng: 31.224020},
-        description: "The Cairo Opera House, part of Cairo's" +
-        " National Cultural Center, is the main performing arts" +
-        " venue in the Egyptian capital."
-    },
-    {
-        place_name: "Opera Metro Station",
-        location: {lat: 30.041842, lng: 31.225215},
-        description: "The Cairo Metro is the rapid transit system" +
-        " in Greater Cairo, Egypt. It was the first of only two ..." +
-        " As of 2014, the Cairo Metro has 61 stations (mostly At-grade)," +
-        " of which 3 are transfer stations, with a total length of 77.9 kilometres"
-    },
-    {
-        place_name: "Tahrir Square",
-        location: {lat: 30.044261, lng: 31.235905},
-        description: "Tahrir Square also known as \"Martyr Square\"," +
-        " is a major public town square in Downtown Cairo, Egypt." +
-        " The square has been the location and focus for political" +
-        " demonstrations in Cairo, most notably those that led to " +
-        "the 2011 Egyptian revolution and the resignation of" +
-        " President Hosni Mubarak."
-    },
-    {
-        place_name: "Egyptian Museum",
-        location: {lat: 30.047855, lng: 31.233572},
-        description: "The Museum of Egyptian Antiquities, known" +
-        " commonly as the Egyptian Museum or Museum of Cairo, in" +
-        " Cairo, Egypt, is home to an extensive collection of" +
-        " ancient Egyptian antiquities."
-    },
-    {
-        place_name: "Sheraton Cairo Hotel & Casino",
-         location: {lat: 30.039232, lng: 31.220301},
-         description: "On the bank of the Nile, the Sheraton" +
-         " Cairo Hotel is minutes away from the Pyramids of" +
-         " Giza and the Egyptian Museum. Book direct for best rates."
-    }
-];
-
-
 // Create a new blank array for all markers.
 var markers = [];
-
+var locationInfowindow;
 // Constructor creates a new map
 function initMap() {
     map = new google.maps.Map(document.getElementById("my_map"), {
@@ -85,18 +15,19 @@ function initMap() {
     });
 
     // Create blank infowindow with max width of 370
-    var location_Infowindow = new google.maps.InfoWindow({maxWidth: 370});
+    locationInfowindow = new google.maps.InfoWindow({maxWidth: 370});
 
     // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < nearby_places.length; i++) {
-        // Get the position, place_name & description from the nearby_places array.
-        var position = nearby_places[i].location;
-        var place_name = nearby_places[i].place_name;
-        var description = nearby_places[i].description;
+    for (var i = 0; i < nearbyPlaces.length; i++) {
+        // Get the position, placeName & description from the nearbyPlaces array.
+        var position = nearbyPlaces[i].location;
+        var placeName = nearbyPlaces[i].placeName;
+        var description = nearbyPlaces[i].description;
         // create message to appear on each infowindow
         // according to the marker selected
         var contentString = "<div id='content'>"+
-            "<h4 id='firstHeading' class='firstHeading'>"+ place_name + "</h4>"+
+            "<span id='errorMessage'></span>" +
+            "<h4 id='firstHeading' class='firstHeading'>"+ placeName + "</h4>"+
             "<div id='bodyContent'>"+
             "<p>" + description + " </p>"+
             "</div>"+
@@ -104,7 +35,7 @@ function initMap() {
         // Create a marker per location, and put into markers array.
         var marker = new google.maps.Marker({
             position: position,
-            place_name: place_name,
+            placeName: placeName,
             description: contentString,
             animation: google.maps.Animation.DROP,
             id: i
@@ -112,17 +43,36 @@ function initMap() {
         // Push the marker to markers array.
         markers.push(marker);
         // Create an onclick event to each marker to open an infowindow
-        createClickEvent(marker, location_Infowindow);
+        createClickEvent(marker, locationInfowindow);
 
     }
     // Display all markers on page load
     showListings();
 }
 
-function createClickEvent(marker, location_Infowindow){
+
+// Marker click event
+function createClickEvent(marker, locationInfowindow){
     marker.addListener("click", function() {
-        populateInfoWindow(this, location_Infowindow);
+        populateInfoWindow(this, locationInfowindow);
     });
+}
+
+// Play animation to a marker when it is clicked
+// or when a place in the list is selected
+function animate(marker){
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+// Stop marker animation
+function stopAnimation(marker){
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    }
 }
 
 // This function populates the infowindow when the marker is clicked.
@@ -132,8 +82,9 @@ function populateInfoWindow(marker, infowindow) {
     // Check whether this marker's infowindow is opened or not
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        console.log(marker.place_name);
-        $.getJSON(filcker+marker.place_name+"&jsoncallback=?",function(data){
+        animate(marker);
+        console.log(marker.placeName);
+        $.getJSON(filcker+marker.placeName+"&jsoncallback=?",function(data){
             var photo = null;
             if(data.photos.photo[0] !== null){
                 console.log(data.photos.photo[0].id);
@@ -162,6 +113,8 @@ function populateInfoWindow(marker, infowindow) {
             var err = textStatus + ", " + error;
             console.log( "Request Failed: " + err );
             console.log( "error" );
+            document.getElementById("errorMessage").innerHTML = "Request failed: " + err;
+
         })
         .always(function() {
             console.log( "complete" );
@@ -170,6 +123,7 @@ function populateInfoWindow(marker, infowindow) {
       // clear marker whem infowindow is closed.
       infowindow.addListener("closeclick", function() {
         infowindow.marker = null;
+        stopAnimation(marker);
       });
     }
 }
@@ -182,18 +136,27 @@ function showListings() {
         markers[i].setMap(map);
         bounds.extend(markers[i].position);
     }
-    map.fitBounds(bounds);
+    google.maps.event.addDomListener(window, "resize", function() {
+        map.fitBounds(bounds);
+    });
+}
+
+// Map api call fail
+function mapFail(){
+     document.getElementById("my_map").innerHTML = "<p class='text-center'>Google maps API could not be loaded!</p>";
 }
 
 // Add only clicked place's marker to map
 var newPlace = function(data){
-    this.name = ko.observable(data.place_name);
+    this.name = ko.observable(data.placeName);
     this.nameClicked = function(clickedData){
         for(var i = 0; i < markers.length; i++){
-            if (markers[i].place_name.toLowerCase().startsWith(clickedData.name().toLowerCase())) {
-                markers[i].setMap(map);
+            if (markers[i].placeName.toLowerCase().startsWith(clickedData.name().toLowerCase())) {
+                markers[i].setVisible(true);
+                populateInfoWindow(markers[i], locationInfowindow);
+                viewModel.Query(markers[i].placeName);
             }else{
-                markers[i].setMap(null);
+                markers[i].setVisible(false);
             }
         }
     };
@@ -201,14 +164,14 @@ var newPlace = function(data){
 
 // create viewModel with KO
 var viewModel = {
-    places:[new newPlace(nearby_places[0]),
-    new newPlace(nearby_places[1]),
-    new newPlace(nearby_places[2]),
-    new newPlace(nearby_places[3]),
-    new newPlace(nearby_places[4]),
-    new newPlace(nearby_places[5]),
-    new newPlace(nearby_places[6]),
-    new newPlace(nearby_places[7])
+    places:[new newPlace(nearbyPlaces[0]),
+    new newPlace(nearbyPlaces[1]),
+    new newPlace(nearbyPlaces[2]),
+    new newPlace(nearbyPlaces[3]),
+    new newPlace(nearbyPlaces[4]),
+    new newPlace(nearbyPlaces[5]),
+    new newPlace(nearbyPlaces[6]),
+    new newPlace(nearbyPlaces[7])
     ]
 };
 
@@ -219,10 +182,10 @@ viewModel.searchResults = ko.computed(function() {
     var q = viewModel.Query();
     console.log(q);
     for(var i = 0; i < markers.length; i++){
-        if (markers[i].place_name.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
-            markers[i].setMap(map);
+        if (markers[i].placeName.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
+            markers[i].setVisible(true);
         }else{
-            markers[i].setMap(null);
+            markers[i].setVisible(false);
         }
     }
     return viewModel.places.filter(function(i) {
@@ -240,11 +203,6 @@ $("#search-input").mouseenter(function(){
     $("#search-input").focusin(function(){
         $(".bd-search-results").css("display", "block");
     });
-});
-
-$(".dropdown-item").click(function(){
-    var placeholder = $(this).text();
-    $("#search-input").attr("placeholder", placeholder);
 });
 
 $("#search-input").focusout(function(){
---
+++ js/places.js
@@ -0,0 +1,69 @@
+
+// Chosen places that will appear on map
+// Those places are selected in Cairo, Egypt
+var nearbyPlaces = [
+    {
+        placeName: "Al-Ahly Sporting Club",
+        location: {lat: 30.048484, lng:  31.222244},
+        description: "Al Ahly Sporting Club, is an Egyptian sports club based in Cairo," +
+        " Egypt. It is best known for its professional football team, " +
+        "which plays in the Egyptian Premier League, the top tier in " +
+        "the Egyptian football league system."
+    },
+    {
+        placeName: "Cairo Tower",
+        location: {lat: 30.045930, lng: 31.224250},
+        description: "The Cairo Tower - commonly known to locals" +
+        " as \"Nasser's Pineapple\" - is a free-standing concrete" +
+        " tower located in Cairo, Egypt. At 187 m, it has been the" +
+        " tallest structure in Egypt and North Africa for about 50 years."
+    },
+    {
+        placeName: "El sawy Culture Wheel",
+        location: {lat: 30.061957, lng: 31.217200},
+        description: "El Sawy Culture Wheel is an all-purpose, private" +
+        " cultural center, located on Gezira Island in the Zamalek" +
+        " district, central Cairo, Egypt. It is considered one of " +
+        "the most important cultural venues in Egypt."
+    },
+    {
+        placeName: "Cairo Opera House",
+        location: {lat: 30.042779, lng: 31.224020},
+        description: "The Cairo Opera House, part of Cairo's" +
+        " National Cultural Center, is the main performing arts" +
+        " venue in the Egyptian capital."
+    },
+    {
+        placeName: "Opera Metro Station",
+        location: {lat: 30.041842, lng: 31.225215},
+        description: "The Cairo Metro is the rapid transit system" +
+        " in Greater Cairo, Egypt. It was the first of only two ..." +
+        " As of 2014, the Cairo Metro has 61 stations (mostly At-grade)," +
+        " of which 3 are transfer stations, with a total length of 77.9 kilometres"
+    },
+    {
+        placeName: "Tahrir Square",
+        location: {lat: 30.044261, lng: 31.235905},
+        description: "Tahrir Square also known as \"Martyr Square\"," +
+        " is a major public town square in Downtown Cairo, Egypt." +
+        " The square has been the location and focus for political" +
+        " demonstrations in Cairo, most notably those that led to " +
+        "the 2011 Egyptian revolution and the resignation of" +
+        " President Hosni Mubarak."
+    },
+    {
+        placeName: "Egyptian Museum",
+        location: {lat: 30.047855, lng: 31.233572},
+        description: "The Museum of Egyptian Antiquities, known" +
+        " commonly as the Egyptian Museum or Museum of Cairo, in" +
+        " Cairo, Egypt, is home to an extensive collection of" +
+        " ancient Egyptian antiquities."
+    },
+    {
+        placeName: "Sheraton Cairo Hotel & Casino",
+         location: {lat: 30.039232, lng: 31.220301},
+         description: "On the bank of the Nile, the Sheraton" +
+         " Cairo Hotel is minutes away from the Pyramids of" +
+         " Giza and the Egyptian Museum. Book direct for best rates."
+    }
+];
